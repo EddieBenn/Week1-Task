2eebfd39609dde2b811afccc4c7529121b7718c1
EddieBenn <ndaobongedidiong56@gmail.com>
2022-05-30 15:39:46 +0100

    Update

--- script/index.js
+++ script/index.js
@@ -29,37 +29,3 @@ async function getPeople(url){
 }
 
 getPeople(baseUrl)
-
-
-
-
-
-
-
-// async function asyncFetch(value) {
-//     const res = await fetch (`https://swapi.co/api/${value}/`)
-//     const data = await res.json();
-//     displayResults(data, value)
-// }
-
-// function displayResults (data, value) {
-//     let output = "";
-//     if(value === "films") {
-//         data.results.forEach(item => {
-//             output += `
-//                 <div class="card p-3 m-3" style="opacity:.8">
-//                     <h4 class"card-title text-center">${item.title}</h4>
-//                 </div>
-//             `
-
-//         });
-
-//     }
-//     results.innerHTML = output;
-// }
-
-
-
-// document.querySelector('#buttons').addEventListener('click', e => {
-//     asyncFetch(e.target.textContent.trim().toLowerCase());
-// }) 
